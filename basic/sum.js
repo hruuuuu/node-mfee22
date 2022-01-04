ab8757e7eb371c45af4f01b26d436d5a6029f40a
Allie <huaruhsiao.b@gmail.com>
2022-01-05 00:32:33 +0800

    [UPDATE] 1. sum.js stress testing 2.readme homework link

--- basic/sum.js
+++ basic/sum.js
@@ -1,4 +1,17 @@
-const sum = (n) => {
+function sumFor(n) {
+  let result = 0;
+  for (let i = 1; i <= n; i++) {
+    result += i;
+  }
+  return result;
+}
+
+function sumFormula(n) {
+  let result = ((1 + n) * n) / 2;
+  return result;
+}
+
+const sumReducer = (n) => {
   arr = [];
   const reducer = (previousValue, currentValue) => previousValue + currentValue;
   for (i = 1; i <= n; i++) {
@@ -8,7 +21,37 @@ const sum = (n) => {
   return result;
 };
 
-console.log(sum(1)); //1
-console.log(sum(3)); //1+2+3=6
-console.log(sum(5)); //1+2+3+4+5=15
-console.log(sum(15)); //1+2+3+4+5+6+7+8+9+10+11+12+13+14+15=120
+function sumRecursive(n) {
+  if (n < 1) {
+    return 0;
+  }
+  return n + sumRecursive(n - 1);
+}
+
+console.time('for');
+for (let i = 1; i <= 10000; i++) {
+  //sumFor(100000); //=> 958.962ms
+  sumFor(100); //=> 11.428ms
+}
+console.timeEnd('for');
+
+console.time('formula');
+for (let i = 1; i <= 10000; i++) {
+  //sumFormula(100000); //=> 0.424ms
+  sumFormula(100); //=> 0.326ms
+}
+console.timeEnd('formula');
+
+console.time('reducer');
+for (let i = 1; i <= 10000; i++) {
+  //sumReducer(100000); //=> 7.196s
+  sumReducer(100); //=> 8.495ms
+}
+console.timeEnd('reducer');
+
+console.time('recursive');
+for (let i = 1; i <= 10000; i++) {
+  //sumRecursive(100000); //=> maximum call stack size exceeded
+  sumRecursive(100); //=> 10.105ms
+}
+console.timeEnd('recursive');

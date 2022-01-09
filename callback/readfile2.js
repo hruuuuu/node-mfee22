5d0e4aea64f7f0561ab7e2297bd651ffca6a43d1
Allie <huaruhsiao.b@gmail.com>
2022-01-09 10:11:03 +0800

    [UPDATE] readfile promise as variable version

--- callback/readfile2.js
+++ callback/readfile2.js
@@ -1,19 +1,39 @@
 const { readFile } = require('fs');
 
-let data = 'test.txt';
-let readFilePromise = (data) => {
-  return new Promise((resolve, reject) => {
-    readFile(data, 'utf-8', (err, data) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(data);
-    });
-  });
-};
+//data用帶入的function版本
+// let data = 'test.txt';
+// let readFilePromise = (data) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(data, 'utf-8', (err, data) => {
+//       if (err) {
+//         reject(err);
+//         return;
+//       }
+//       resolve(data);
+//     });
+//   });
+// };
 
-readFilePromise(data)
+// readFilePromise(data)
+//   .then((result) => {
+//     console.log(`這裡是promise的result: ${result}`);
+//   })
+//   .catch((err) => {
+//     console.log(`這裡是promise的err: ${err}`);
+//   });
+
+//直接把promise存成變數的版本
+let readFilePromise = new Promise((resolve, reject) => {
+  readFile('test.txt', 'utf-8', (err, data) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve(data);
+  });
+});
+
+readFilePromise
   .then((result) => {
     console.log(`這裡是promise的result: ${result}`);
   })

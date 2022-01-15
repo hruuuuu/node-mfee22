d07a0b2bf0c1a2c2cf0b83b2440ab846c9152632
Allie <huaruhsiao.b@gmail.com>
2022-01-15 13:16:05 +0800

    [UPDATE] query date by moment

--- crawler/crawlers3.js
+++ crawler/crawlers3.js
@@ -1,24 +1,30 @@
 const axios = require('axios');
 const { readFile } = require('fs/promises');
+const moment = require('moment');
 
 (async () => {
   try {
     const stock = await readFile('stock.txt', 'utf-8');
-    const now = new Date;
-    const month = Number(now.getMonth() + 1);
-    const date = Number(now.getDate());
-    const format = (num)=>{
-      if (num < 10){
-        return `0${num}`;
-      }else{
-        return num;
-      }
-    }
-    const today = `${now.getFullYear()}${format(month)}${format(date)}`;
+    /*自動改成今天的日期
+    // const now = new Date;
+    // const month = Number(now.getMonth() + 1);
+    // const date = Number(now.getDate());
+    // const format = (num)=>{
+    //   if (num < 10){
+    //     return `0${num}`;
+    //   }else{
+    //     return num;
+    //   }
+    // }
+    // const today = `${now.getFullYear()}${format(month)}${format(date)}`;
     //console.log(today);
+    //let queryDate = today; 
+    */
+
     //try catch只要包一層就可以了
+
       let stockNo = stock;
-      let queryDate = today; //自動改成今天的日期
+      let queryDate = moment().format("YYYYMMDD");
       const response = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY",
       {
         params:{

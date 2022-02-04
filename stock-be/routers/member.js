be08bf1c2e16183e6c7b6bc199e010c373d9186c
Allie <huaruhsiao.b@gmail.com>
2022-02-05 03:24:44 +0800

    [UPDATE] get member data if login

--- stock-be/routers/member.js
+++ stock-be/routers/member.js
@@ -7,16 +7,28 @@ const { checkLogin } = require('../middlewares/auth');
 router.use(checkLogin);
 
 router.get('/', (req, res, next) => {
+  /* 改寫成checkLogin middleware require即可
+  //如果session裡有member存在(有登入過)
   // if (req.session.member) {
+  //   //就回傳member資料
   //   res.json(req.session.member);
   // } else {
+  //   //否則回傳錯誤訊息
   //   req.statusCode(400).json({
   //     code: 99999,
   //     msg: '尚未登入',
   //   });
   // }
+  */
 
-  //經過checkLogin認證登入了 就可以直接拿到資料
+  //經過checkLogin確認登入了 就可以直接拿到資料
   res.json(req.session.member);
+
+  /*
+  res.json({
+    id: 1,
+    name: '小賴',
+  })
+  */
 });
 module.exports = router;
